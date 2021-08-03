19f96568d5096599374a98492866069bcb1eb137
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-08-03 11:55:53 -0500

    update develop

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -8,7 +8,7 @@ const { route } = require('./user-routes');
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [[ 'created_at', 'DESC']]
+        order: [[ 'created_at', 'DESC']],
         include: [
             {
               model: User,
